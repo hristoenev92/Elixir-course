94a3a971a6eab26e3f2f6dfccdd347f78cbb0b32
hristoenev92 <hristoenev92@gmail.com>
2018-08-03 15:02:09 +0300

    commiting finished sixth homework
    some renames to homework5

--- lesson5-homework.ex
+++ lesson5-homework.ex
@@ -1,57 +1,57 @@
 defmodule Homework5 do
 
   #Write a recursive definition, that calculates the sum of list of integers. E.g. if you pass it [1, 2, 3, 4], it should return 10
-  def listSum([head|tail]) do
-    listSum(tail, head)
+  def list_sum([head|tail]) do
+    list_sum(tail, head)
   end
-  def listSum([head|tail], sum) do
-    listSum(tail, head+sum)
+  def list_sum([head|tail], sum) do
+    list_sum(tail, head+sum)
   end
-  def listSum([], sum) do
+  def list_sum([], sum) do
     sum
   end
 
   # Write a recursive definition of “length/1” (the function that calculates the length of a list)
 
-  def listLenght([head|tail]) do
-    listLenght(tail, 1)
+  def list_lenght([head|tail]) do
+    list_lenght(tail, 1)
   end
-  def listLenght([head|tail], lenght) do
-    listLenght(tail, lenght+1)
+  def list_lenght([head|tail], lenght) do
+    list_lenght(tail, lenght+1)
   end
-  def listLenght([], lenght) do
+  def list_lenght([], lenght) do
     lenght
   end
 
   #Write a recursive function, that calculates the sum of all even items in a list (to compute the remainder of an integer division, you can use the function rem/2)
 
-  def sumEven([head|tail]) do
-    sumEven([head|tail], 0)
+  def sum_even([head|tail]) do
+    sum_even([head|tail], 0)
   end
-  def sumEven([head|tail], sum) do
+  def sum_even([head|tail], sum) do
     if rem(head, 2) ==0 do
-      sumEven(tail, sum+head)
+      sum_even(tail, sum+head)
     else
-       sumEven(tail,sum)
+       sum_even(tail,sum)
     end
   end
-  def sumEven([], sum) do
+  def sum_even([], sum) do
     sum
   end
 
   #Write a recursive function, that takes a list of strings, and return the uppercase version of all the strings, that start with “a” (case-insensitive)
 
-  def listToUpper([head|tail]) do
-    listToUpper([head|tail],[])
+  def list_to_upper([head|tail]) do
+    list_to_upper([head|tail],[])
   end
-  def listToUpper([head|tail], newList) do
+  def list_to_upper([head|tail], newList) do
     if String.at(head, 0) == "a" || String.at(head, 0) == "A" do
-      listToUpper(tail, newList++[String.upcase(head)])
+      list_to_upper(tail, newList++[String.upcase(head)])
     else
-      listToUpper(tail, newList)
+      list_to_upper(tail, newList)
     end
   end
-  def listToUpper([], newList)do
+  def list_to_upper([], newList)do
     newList
   end
 
--- lesson6-homework.ex
+++ lesson6-homework.ex
@@ -17,16 +17,70 @@ end end)
 
 
 defmodule Homework6 do
-	#Write a function, that tests if a list is empty, and returns `true`, `false`, or “This is not a list!”
+		#Write a function, that tests if a list is empty, and returns `true`, `false`, or “This is not a list!”
 	
-def empty_list(list) when is_list(list) do
- if list==[] do true else false end
-end
-def empty_list(list) when not is_list(list) do "This is not a list!"
-end
+	def empty_list(list) when is_list(list) do
+	 if list==[] do true else false end
+	end
+	def empty_list(list) when not is_list(list) do "This is not a list!"
+	end
 
-	#Write a recursive function, that calculates the sum of all even numbers in a list, using guards
+		#Write a recursive function, that calculates the sum of all even numbers in a list, using guards
 
+	def sum_even(list) do
+		Enum.reduce(list, 0, fn x, acc ->
+			if Integer.is_even(x) do acc+x else acc end
+		end) 
+	end
+
+	def sum_even_recursive([head|tail]) do
+		sum_even_recursive([head|tail], 0)
+	end
+	def sum_even_recursive([head|tail], sum) do
+		if Integer.is_even(head) do
+			sum_even_recursive(tail, sum+head)
+		else
+			sum_even_recursive(tail,sum)
+		end
+	end
+	def sum_even_recursive([], sum) do
+		sum
+	end 
+
+		#on a list of numbers, return all of them doubled
+	def double_list(list) do
+		Enum.reduce(list, [], fn x, acc ->
+		 [x*2|acc]
+		end)
+	end	
+	
+		#on a list of numbers, return a list, consisting of the even ones, doubled
+	def double_evens(list) do	
+		Enum.reduce(list, [], fn x, acc ->
+			if Integer.is_even(x) do [x*2|acc] else acc end
+		end)
+	end
 end
-
-//To be continued...
+		#The task is, to write a refrigerator application. It should be possible to add items to it, to remove items from it, to keep count of the number of products, that are the same:
+		#Write a module `Refrigerator`
+defmodule Refrigerator do
+		
+		#It should have a function `new`, that returns a new empty refrigerator, represented as an empty map
+	def new() do
+		%{}
+	end
+	
+		#It should have a function `add`, that takes a refrigerator, an item and count, and returns a new refrigerator, that has the count to the appropriate key in the map. Otherwise it should add a new key to the refrigerator.
+	def add(refrigerator, item, count) do
+		if Map.has_key?(refrigerator, item) do
+			Map.update!(refrigerator, item, &(&1 + count))
+		else
+			Map.put(refrigerator, item, count)
+		end
+	end
+	
+		#It should have a function `remove`, that takes a refrigerator, an item and count, and returns a new refrigerator, with the count removed from the key in the map. If the count is more than the number of items in the fridge, it should leave the item to 0
+	def remove(refrigerator, item, count) do
+		Map.update!(refrigerator, item, &(if (&1-count)<0 do 0 else (&1-count) end))
+	end
+end
