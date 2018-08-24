fedc55cc4bbbaccaae12f2b944c727d6d3c039e6
hristoenev92 <hristoenev92@gmail.com>
2018-08-24 17:48:02 +0300

    commiting homework 12 with changes to homework 11

--- lesson11-homework.ex
+++ lesson11-homework.ex
@@ -1,15 +1,17 @@
 defmodule ReceiveExample do
-
-  def loop do
+# TODO new type of message - :foo, (only the atom :foo), which after being received prints "Done the first task"
+# TODO default clause - catch all, toest :) and prints "Done the second task"
+# TODO be able to receive unlimited messages
+# TODO return a message
+  def loop() do
     receive do
       {:answer, text} -> IO.puts(text)
       {:test, text} -> IO.puts("received " <> text)
-      {sender, message} -> send(self(), {:answer, "Received: " <> message})
+      {sender, message} -> send(sender, {:answer, "Received: " <> message})
       :foo -> IO.puts("Done the first task")
       _ -> IO.puts("Done the second task")
-    after
-      1000->"recieved nothing"
     end
+    loop()
   end
-
+  
 end
